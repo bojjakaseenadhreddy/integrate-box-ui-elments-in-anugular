3111d00935e43e8a9dc9f68c591ef80feff6a43a
kaseenadh bojja <kaseenadh111@gmail.com>
2020-11-05 12:37:34 +0530

    Added CDN links and environment variables.

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,9 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  boxAccessToken: "XknQDM06ErQkfwFFGCywdIdPLHG1Yf1v",
+  boxFolderId: "0"
 };
 
 /*
